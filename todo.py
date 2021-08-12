3644929306b2098591b61778fe8256d264ad0716
Mims20 <psterlings@gmail.com>
2021-08-12 18:20:56 -0400

    Add files via upload

---
+++ todo.py
@@ -0,0 +1,14 @@
+# TODO 1
+'''Move the turtle with keypress'''
+
+# TODO 2
+'''Create and move the cars'''
+
+# TODO 3
+'''Detect collision with car'''
+
+# TODO 4
+'''Detect when turtle reaches the other side'''
+
+# TODO 5
+'''Create a scoreboard'''
